3730fa8fdf25c8ec6c79a7934489f8729c14f488
kazuaaaaaaaaaaa <sugakazu1121@gmail.com>
2024-12-30 06:47:58 +0900

    app1.js編集

--- app1.js
+++ app1.js
@@ -3,6 +3,7 @@ const http = require('http');
 var server = http.createServer(
   (request,response)=>{
     response.end('Hello Node.js!');
+    response.end('This is my first code');
   }
 );
 server.listen(3000);
