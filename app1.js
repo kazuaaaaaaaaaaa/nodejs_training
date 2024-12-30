2dc99ff983c9cad69b08744284b45e544e8d2ba5
kazuaaaaaaaaaaa <sugakazu1121@gmail.com>
2024-12-31 06:17:46 +0900

    app1

--- app1.js
+++ app1.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 var server = http.createServer(
   (request,response)=>{
-    response.write('<title>Hello</title></head>');
+    response.write('<title>Hello</title>');
     response.end('Hello Node.js!');
     response.end('This is my first code');
   }
